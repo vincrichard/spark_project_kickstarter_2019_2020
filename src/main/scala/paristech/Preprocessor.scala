d47e19733a1f1ff1ba2907eaa1a5e6c416c3ca65
vincrichard <vincent.richard1094@gmail.com>
2019-10-17 12:03:10 +0200

    TP2 done

--- src/main/scala/paristech/Preprocessor.scala
+++ src/main/scala/paristech/Preprocessor.scala
@@ -1,7 +1,10 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
+
 
 object Preprocessor {
 
@@ -28,6 +31,8 @@ object Preprocessor {
       .appName("TP Spark : Preprocessor")
       .getOrCreate()
 
+    import spark.implicits._
+
     /*******************************************************************************
       *
       *       TP 2
@@ -40,8 +45,145 @@ object Preprocessor {
       *
       ********************************************************************************/
 
-    println("\n")
-    println("Hello World ! from Preprocessor")
-    println("\n")
+    /****************************************
+      * Charger un fichier csv dans un dataFrame
+      *********************************************/
+
+    val df: DataFrame = spark
+      .read
+      .option("header", true) // utilise la première ligne du (des) fichier(s) comme header
+      .option("inferSchema", "true") // pour inférer le type de chaque colonne (Int, String, etc.)
+      .csv("/media/vincent/C0FC3B20FC3B0FE0/MSBGD/Spark/TP/cours-spark-telecom/data/train_clean.csv")
+
+//    println(s"Nombre de lignes : ${df.count}")
+//    println(s"Nombre de colonnes : ${df.columns.length}")
+//    df.show()
+//    df.printSchema()
+
+    val dfCasted: DataFrame = df
+      .withColumn("goal", $"goal".cast("Int"))
+      .withColumn("deadline" , $"deadline".cast("Int"))
+      .withColumn("state_changed_at", $"state_changed_at".cast("Int"))
+      .withColumn("created_at", $"created_at".cast("Int"))
+      .withColumn("launched_at", $"launched_at".cast("Int"))
+      .withColumn("backers_count", $"backers_count".cast("Int"))
+      .withColumn("final_status", $"final_status".cast("Int"))
+
+//    dfCasted.printSchema()
+
+//    dfCasted
+//      .select("goal", "backers_count", "final_status")
+//      .describe()
+//      .show
+
+//    dfCasted.groupBy("disable_communication").count.orderBy($"count".desc).show(100)
+//    dfCasted.groupBy("country").count.orderBy($"count".desc).show(100)
+//    dfCasted.groupBy("currency").count.orderBy($"count".desc).show(100)
+//    dfCasted.select("deadline").dropDuplicates.show()
+//    dfCasted.groupBy("state_changed_at").count.orderBy($"count".desc).show(100)
+//    dfCasted.groupBy("backers_count").count.orderBy($"count".desc).show(100)
+//    dfCasted.select("goal", "final_status").show(30)
+//    dfCasted.groupBy("country", "currency").count.orderBy($"count".desc).show(50)
+    //Remove a column which is not useful
+    val df2: DataFrame = dfCasted.drop("disable_communication")
+    //Remove column with values concerning the future result of the campaign
+    val dfNoFutur: DataFrame = df2.drop("backers_count", "state_changed_at")
+
+//    df.filter($"country" === "False")
+//      .groupBy("currency")
+//      .count
+//      .orderBy($"count".desc)
+//      .show(50)
+
+    //
+    val cleanCountryUdf = udf(cleanCountry _)
+    val cleanCurrencyUdf = udf(cleanCurrency _)
+
+    val dfCountry: DataFrame = dfNoFutur
+      .withColumn("country2", cleanCountryUdf($"country", $"currency"))
+      .withColumn("currency2", cleanCurrencyUdf($"currency"))
+      .drop("country", "currency")
+
+    // ou encore, en utilisant sql.functions.when:
+//    import sql.functions.when
+//    dfNoFutur
+//      .withColumn("country2", when($"country" === "False", $"currency").otherwise($"country"))
+//      .withColumn("currency2", when($"country".isNotNull && length($"currency") =!= 3, null).otherwise($"currency"))
+//      .drop("country", "currency")
+
+    //Keep the meaningful final_status variable
+    val dfFinalStatus = dfCountry
+      .filter($"final_status" === 1 or $"final_status" === 0)
+
+    //Ajout d'une colone days_campaign contenant la durée de la campagne
+    val createDaysCampaignUdf = udf(createDaysCampaign _)
+    val dfDayCampaign : DataFrame = dfFinalStatus
+      .withColumn("days_campaign", createDaysCampaignUdf($"launched_at", $"deadline"))
+    //Ajout d'une colonne hours_prepa contenant le temps de preparation de la campagne
+    val createHoursPrepaUdf = udf(createHoursPrepa _)
+    val dfHoursPrepa : DataFrame = dfDayCampaign
+      .withColumn("hours_prepa", round(createHoursPrepaUdf($"created_at", $"launched_at"), 3))
+      .drop("launched_at", "created_at", "launched_at")
+
+    //Modification des champs de texte
+    val dfText = dfHoursPrepa
+      .withColumn("name", lower($"name"))
+      .withColumn("desc", lower($"desc"))
+      .withColumn("keywords", lower($"keywords"))
+
+    val dfConcatenate = dfText
+      .withColumn("text", concat_ws(" ", $"name", $"desc", $"keywords"))
+      .drop("name", "desc", "keywords")
+
+    //Remove null values
+    val dfClean = dfConcatenate
+      .na.fill(-1, Array("days_campaign", "hours_prepa", "goal"))
+      .na.fill("unknown", Array("country2", "currency2"))
+    //Remove hours_prepa < 0
+      .filter($"hours_prepa" >0)
+
+    //Create parquet file
+    dfClean.write.mode(SaveMode.Overwrite).parquet("/media/vincent/C0FC3B20FC3B0FE0/MSBGD/Spark/TP/data/")
   }
+
+  /**
+    * Methode utilisé pour Clean la colonne county
+    */
+  def cleanCountry(country: String, currency: String): String = {
+    if (currency != null && (country == "False" || country.length != 2))
+      null
+    else
+      country
+  }
+
+  /**
+    * Methode utilisé pour Clean la colonne currency
+    */
+  def cleanCurrency(currency: String): String = {
+    if (currency != null && currency.length != 3)
+      null
+    else
+      currency
+  }
+
+  /**
+    * Methode utilisé pour créer la colonne days_campaign
+    * @param launched_at
+    * @param deadline
+    * @return
+    */
+  def createDaysCampaign(launched_at: Int, deadline: Int): Int = {
+    return (deadline - launched_at) / 86400
+  }
+
+  /**
+    * Methode utilisé pour créer la colonne hours_prepa
+    * @param created_at
+    * @param launched_at
+    * @return
+    */
+  def createHoursPrepa(created_at: Int, launched_at: Int): Double = {
+    return (launched_at.toFloat - created_at.toFloat) / 3600.0
+  }
+
 }
